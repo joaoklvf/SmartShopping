91e9d9888e895d20c57aebe9603289fecf267a2a
João Figueredo <joaoklvf@gmail.com>
2025-05-17 17:18:50 -0300

    feat(product-card): accessibility

--- app/components/products/product-card/product-card.tsx
+++ app/components/products/product-card/product-card.tsx
@@ -7,16 +7,31 @@ export function ProductCard({ product }: ProductCardProps) {
   const amount = getAmountStr(quantity);
 
   return (
-    <div className="flex flex-col w-[300px] h-[500px] bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <img className="flex-none rounded-t-lg w-[300px] h-[300px] object-cover overflow-auto" src={imgUrl} alt="" />
-      <div className='m-4 h-full flex flex-col justify-between'>
+    <article
+      className="flex flex-col w-[300px] h-[500px] bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+      aria-label={`Produto: ${name}`}
+    >
+      <img
+        className="flex-none rounded-t-lg w-[300px] h-[300px] object-cover"
+        src={imgUrl}
+        alt={`Imagem do produto ${name}`}
+      />
+      <div className="m-4 h-full flex flex-col justify-between">
         <div>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-          <p>{currencyPrice}</p>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Quantidade disponível: {amount}</p>
+          <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h3>
+          <p className="text-gray-900 dark:text-white text-xl">{currencyPrice}</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Quantidade disponível: {amount}
+          </p>
         </div>
-        <button type="button" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-center">Adicionar ao carrinho</button>
+        <button
+          type="button"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-center"
+          aria-label={`Adicionar ${name} ao carrinho`}
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
-    </div>
+    </article>
   );
 }
