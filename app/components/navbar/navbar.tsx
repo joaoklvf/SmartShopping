61f489ddbebd3dae92ca9df5f2a47e43f86cf957
João V <joaoklvf@gmail.com>
2025-05-19 11:24:54 -0300

    feat: creating and using cart icon

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CartIcon } from "./cart-icon";
 
 export function Navbar() {
   const [isMobileOpened, setIsMobileOpened] = useState(false)
@@ -37,9 +38,7 @@ export function Navbar() {
             <button type="button" className="cursor-pointer relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">View notifications</span>
-              <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-              </svg>
+              <CartIcon className="size-6" />
             </button>
           </div>
         </div>
@@ -57,4 +56,4 @@ export function Navbar() {
       </div>
     </nav>
   );
-} 
+}
