accf5e54ea8aa155c27fc4c35995b8558971a058
João V <joaoklvf@gmail.com>
2025-05-19 10:42:02 -0300

    feat: renaming category products session component

--- app/components/products/products.tsx
+++ app/components/products/products.tsx
@@ -1,6 +1,6 @@
 import products from '../../data/products.json';
 import categories from '../../data/categories.json';
-import { CategoryProductsSession } from './category-products-session/product-session';
+import { CategoryProductsSession } from './category-products-session/category-products-session';
 import type { CategoryProducts } from '~/models/category-products';
 
 export function Products() {
